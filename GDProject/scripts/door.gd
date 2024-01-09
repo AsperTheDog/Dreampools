03a27ac19ce9c2a5a88a44ed8cff22990daf0745
AsperTheDog <guillerman0000@gmail.com>
2024-01-09 14:59:33 +0100

    various fixes

--- GDProject/scripts/door.gd
+++ GDProject/scripts/door.gd
@@ -4,8 +4,12 @@ extends Area3D
 @export var white: Node3D
 @export var music: Node3D
 
+var interacted: bool = false
+
 
 func interact():
+	if interacted: return
+	interacted = true
 	white.show()
 	music.finish()
 	door.get_node("AnimationPlayer").play("open-door")
